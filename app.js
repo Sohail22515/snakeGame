676ca98fb3a3cdf67009e8480109e01a1c1199a2
Mohammad Sohail <118095145+Sohail22515@users.noreply.github.com>
2024-06-15 00:06:36 +0530

    Update app.js
    if(){} statement was added in directions 
    so that the snake can't move back on its track again

--- app.js
+++ app.js
@@ -155,28 +155,36 @@ window.addEventListener('keydown',e=>{
     switch(e.key){
         case "ArrowUp":
             // console.log("up");
-            inputDir.x=0;
-            inputDir.y=-1;
+            if(inputDir.y!=1){
+                inputDir.x=0;
+                inputDir.y=-1;
+            }
             break;
 
         case "ArrowDown":
             // console.log("down ");
-            inputDir.x=0;
-            inputDir.y=1;
+            if(inputDir.y!=-1){
+                inputDir.x=0;
+                inputDir.y=1;
+            }
             break;
 
         case "ArrowLeft":
             // console.log("left");
-            inputDir.x=-1;
-            inputDir.y=0;
+            if(inputDir.x!=1){
+                inputDir.x=-1;
+                inputDir.y=0;
+            }
             break;
 
         case "ArrowRight":
             // console.log("right");
-            inputDir.x=1;
-            inputDir.y=0;
+            if(inputDir.x!=-1){
+                inputDir.x=1;
+                inputDir.y=0;
+            }
             break;
-             
+            
         default:
             break;
 
